08f0e4185ea81971dc34f37e070d97f89ae975dc
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-09 11:04:55 +0100

    :recycle: structuring the content components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import DashboardContainer from "./DashboardContainer";
-import ContentContainer from "./ContentContainer";
-import Search from "./Search";
-import Hot from "./Hot";
-import Liked from "./Liked";
-import Current from "./Current";
+import ContentContainer from "./content/ContentContainer";
+import Search from "./content/Search";
+import Hot from "./content/Hot";
+import Liked from "./content/Liked";
+import Current from "./content/Current";
 
 // eslint-disable-next-line react/prop-types
 const Dashboard = ({ search, hot, liked, current }) => {
--- src/components/content/ContentContainer.jsx
+++ src/components/content/ContentContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./Header";
+import Header from "../Header";
 
 // eslint-disable-next-line react/prop-types
 const ContentContainer = ({ children }) => {
   return (
     <div className=" bg-dark flex-1 flex flex-col">
       <Header></Header>
-      {children}
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
