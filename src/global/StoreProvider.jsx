31abf9b5456bdb7cc09b14ac204dc8b5f7da4d1b
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-11 15:05:45 +0100

    :sparkles: setup the global store

---
+++ src/global/StoreProvider.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useMemo } from "react";
+import { storeContext } from "./store";
+
+// eslint-disable-next-line react/prop-types
+const StoreProvider = ({ children }) => {
+  const [store, setStore] = useState({});
+
+  const storeValue = useMemo(() => ({ store, setStore }), [store, setStore]);
+
+  return (
+    <storeContext.Provider value={storeValue}>{children}</storeContext.Provider>
+  );
+};
+
+export default StoreProvider;
