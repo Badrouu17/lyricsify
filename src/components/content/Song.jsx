508d38bb867ce57e0909f79d03ac73c2d9810e1e
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-11 12:29:19 +0100

    :lipsstick: song component

--- src/components/content/Song.jsx
+++ src/components/content/Song.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Card from "./Card";
+import { IconContext } from "react-icons";
+import { IoIosEye, IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
 
 const Song = () => {
-  return <div className="w-1/3 overflow-hidden shadow-lg"></div>;
+  return (
+    <div className="w-1/3 py-32 overflow-hidden shadow-lg flex flex-col">
+      <Card></Card>
+      <div className="flex flex-col text-4xl">
+        <IconContext.Provider value={{ size: "1em", style: { color: "#fff" } }}>
+          <div className="mt-4 flex flex-row items-center justify-center">
+            <span className=" w-1/2"> 10,525,768</span>
+            <IoIosEye></IoIosEye>
+          </div>
+          <div className="mt-4 flex flex-row items-center justify-center">
+            <span className=" w-1/2">Add to liked</span>
+            <IoIosHeartEmpty></IoIosHeartEmpty>
+          </div>
+        </IconContext.Provider>
+      </div>
+    </div>
+  );
 };
 
 export default Song;
