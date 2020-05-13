8ac0b0058eeccc78cf5ed5af6edcdeb46dcf16f5
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-13 09:41:40 +0100

    :sparkles: lyrics logic:

--- src/services/getSongLyrics.js
+++ src/services/getSongLyrics.js
@@ -2,11 +2,10 @@ import Axios from "axios";
 
 export async function getSongsLyrics(url) {
   try {
-    const response = await Axios({
-      method: "GET",
-      url: `https://lyrics-scraper.herokuapp.com/getSongLyrics`,
-      data: { url },
-    });
+    const response = await Axios.post(
+      `https://lyrics-scraper.herokuapp.com/getSongLyrics`,
+      { url }
+    );
 
     return response;
   } catch (error) {
