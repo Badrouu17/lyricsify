19e53251deedf81ac15efe8caba1fd1b6b26bf8d
badreddin labed <badrouu.laabed@gmail.com>
2020-11-06 17:12:08 +0100

    :bug: fix google api key issues

--- src/services/getYoutubId.js
+++ src/services/getYoutubId.js
@@ -6,7 +6,7 @@ export async function getYoutubeId(song, artist) {
   }
   try {
     const res = await request({
-      url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${artist}+${song}+audio&key=AIzaSyBQeoGdrP2jFHTh4a2hy4Q_OHdojWgfQis`,
+      url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${artist}+${song}+audio&key=AIzaSyA2FNl8euTu86bNPcs2qooNyVAayPmrv5o`,
     });
     return res;
   } catch (error) {
