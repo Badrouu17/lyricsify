78a696f311b9bdc61bf5552b2d2b20abff67b396
badreddin labed <badrouu.laabed@gmail.com>
2020-11-06 21:11:08 +0100

    :bug: getting audio fix

--- src/services/getYoutubId.js
+++ src/services/getYoutubId.js
@@ -6,7 +6,7 @@ export async function getYoutubeId(song, artist) {
   }
   try {
     const res = await request({
-      url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${artist}+${song}+audio&key=AIzaSyA2FNl8euTu86bNPcs2qooNyVAayPmrv5o`,
+      url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${artist}+${song}+audio&key=AIzaSyC9SACM1_o90kXXDLWnrLkO6jicXkEfbGA`,
     });
     return res;
   } catch (error) {
